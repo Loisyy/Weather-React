bf00fa2ec0e6e51d0ddea87d3c77fa1b60c1e441
Loisyy <nkeiruloisy@yahoo.com>
2023-10-15 09:02:30 +0100

    Update all

--- src/App.js
+++ src/App.js
@@ -1,18 +1,40 @@
 import React from "react";
 import Weather from "./Weather";
-import './App.css';
+import "./App.css";
 
 export default function App() {
   return (
-    <div className="App">Weather App 
-    <footer>
-      This project was coded by Lois Nkeiru{" "}
-      <a href="https://github.com/Loisyy/Weather-React.git" target="_blank"
-      > 
-      Open sourced on Github
-      </a>
-    </footer>
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="New York" />
+
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/nkeirulois"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Lois Nkeiru
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/Loisyy/Weather-React.git"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            open-sourced on GitHub
+          </a>{" "}
+          and{" "}
+          <a
+            href="https://weather-app-ngle.vercel.app/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            hosted on vercel
+          </a>
+        </footer>
+      </div>
     </div>
-    
   );
 }
